61b1cdafbc0270cabdc2c3d2f68e857cfdc4e109
hakeris1010 <hakeris1010@gmail.com>
2018-02-12 23:18:25 +0200

    Still testing various structural approaches. Trying to find an optimal solution at lowest cost.

--- ex3/main.cpp
+++ ex3/main.cpp
@@ -8,57 +8,133 @@
 #include <cassert>
 #include "execution_time.hpp"
 
-// Structure to store node's data.
-template<class Data>
-struct GraphNode{
-    // A reference to a memory location storing the data of this node.
-    const Data& data;
+template<class Element>
+class LabeledNodeBase {
+protected:
+    LabeledNodeBase( long _label = 0, bool _ready = true ) : 
+        label( _label ), isReady( _ready )
+    {}
 
-    std::vector< std::shared_ptr< GraphNode<Data> > > neighbors;
+public:
+    // Label assigned to node.
     std::atomic< long > label;
 
+    // Synchronization primitives for managing the dynamic loading of data.
     volatile bool isReady;
-    std::condition_variable var;
+    std::condition_variable var; 
 
+    virtual int getNeigbors( std::vector< LabeledNodeBase<Element>* >& neighs ) const = 0;
+    virtual Element& getData() = 0;
+    virtual const Element& getData() const = 0;
+};
+
+// Structure to store node's data.
+template<class Data>
+class GraphNode : public LabeledNodeBase<Data>{
+private:
+    // A reference to a memory location storing the data of this node.
+    const Data& data; 
+     
+    // Pointers to neighboring objects.
+    const std::vector< std::shared_ptr< GraphNode<Data> > > neighbors;
+
+public:
     GraphNode( const Data& _data, 
                std::initializer_list< std::shared_ptr< GraphNode<Data> > >&& _neighs,
                long _label = 0,
                bool _ready = true )
-        : data( _data ), neighbors( std::move( _neighs ) ), label( _label ), isReady( _ready )
+        : data( _data ), neighbors( std::move( _neighs ) ), 
+          LabeledNodeBase<Data>( _label, _ready )
     {}
 
     GraphNode( const Data& _data, 
                std::vector< std::shared_ptr< GraphNode<Data> > >&& _neighs,
                long _label = 0,
                bool _ready = true )
-        : data( _data ), neighbors( std::move( _neighs ) ), label( _label ), isReady( _ready )
+        : data( _data ), neighbors( std::move( _neighs ) ),
+          LabeledNodeBase<Data>( _label, _ready )
+    {}
+
+    int getNeigbors( std::vector< LabeledNodeBase<Data>* >& neighs ) const {
+        for( auto&& n : neighbors ){
+            // Get raw pointers from the shared ptrs.
+            neighs.push_back( n.get() );
+        }
+    }
+
+    const Data& getData() const {
+        return data;
+    }
+};
+
+
+template<class Element, typename Neighborhood>
+class VectorNode : public LabeledNodeBase<Element>{
+private:
+    const std::vector< VectorNode >& container;
+    const size_t index;
+
+    const std::function< void( const std::vector< Element >&, size_t, 
+                   std::vector< LabeledNodeBase<Element>* >& > neighFinder;
+
+public:
+    VectorNode( std::vector< Element >& cont, size_t ind, const Neighborhood lambda,
+                long _label = 0, bool _ready = true ) : 
+        container( cont ), index( ind ), LabeledNodeBase<Element>( _label, _ready )
+    { assert( index <= container.size() ); }
+
+    int getNeigbors( std::vector< LabeledNodeBase<Element>* >& neighs ) const = 0;
+
+    const Element& getData() const {
+        return container[ index ];
+    }
+};
+
+
+// Structure to store node's data.
+template<class Data>
+struct SimpleLabeledNode{
+private:
+    Data data;
+
+public:
+    std::atomic< long > label;
+
+    volatile bool isReady;
+    std::condition_variable var;
+
+    // Copy and move constructors.
+    LabeledNode( const Data& _data, long _label = 0, bool _ready = true )
+        : data( _data ), label( _label ), isReady( _ready )
+    {}
+
+    LabeledNode( Data&& _data, long _label = 0, bool _ready = true )
+        : data( std::move( _data ) ), label( _label ), isReady( _ready )
     {}
 };
 
-template<class Element>
-class GraphNodeBase {
-public:
-    virtual int getAccessibleNeigbors( std::vector< Element& >& neighs ) const = 0;
-    virtual Element& getData() = 0;
-    virtual const Element& getData() const = 0;
-};
-
-
-template<class Element>
-class VectorGraphNode{
+template< class Element >
+class MatrixGraphTraverserConst{
 private:
-    std::vector< Element >& container;
-    size_t index;
-    std::function< int( size_t, const std::vector< Element >&, std::vector<Element>& ) > 
-        getNeighbors;
+    const std::vector< Element >& matrix;
+    const std::vector< size_t > dimensions;
 
 public:
-    VectorGraphNode( std::vector< Element >& cont, size_t ind ) : 
-        container( cont ), index( ind ) {}
+    /*! Copy and move constructors.
+     * @param matrix - reference to an existing vector of elements
+     */ 
+    MatrixGraphTraverser( std::vector< Element >& matrix, 
+                          const std::vector<size_t>& dimensions )
+        : matrix( _matrix ), dimensions( _dimensions )
+    {}
 
+    MatrixGraphTraverser( std::vector< Element >& matrix, 
+                          std::vector<size_t>&& dimensions )
+        : matrix( _matrix ), dimensions( std::move( _dimensions ) )
+    {} 
 
+}
 
-};
 
 
 struct Pixel{
@@ -88,20 +164,9 @@ struct Pixel{
         return abs(red - p.red) <= thr   && abs(green - p.green) <= thr && 
                abs(blue - p.blue) <= thr && abs(alpha - p.alpha) <= thr;
     }
-}
+};
 
-void footest( GraphNode& startNode ){
-    start
-}
-
-/*void boo(){
-    double a = 0.000000001;
-    for(int i=0; i<1000; i++){
-        a += 0.3335/((double)i);
-        double b = a*3.141592;
-    }
-}*/
-
+/*
 void foo( size_t count ){
     // Memory used less than 100 MB.
     assert( count * ( sizeof( std::shared_ptr< GraphNode<int> > ) + 
@@ -129,11 +194,12 @@ void foo2( size_t count ){
         tmp = (tmp->neighbors[0]).get(); 
     }
 }
+*/
 
 int main(){
     size_t count = 100000;
 
-    std::cout<< "foo() took "<< gtools::functionExecTime( foo, count ).count() <<" s\n";
+    //std::cout<< "foo() took "<< gtools::functionExecTime( foo, count ).count() <<" s\n";
     //std::cout<< "foo2() took "<< gtools::functionExecTime( foo2, count ).count() <<" s\n";
 
     return 0;
